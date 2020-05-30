c4a3b59782f5527cffb9a9ee2f5586220f2db5ce
Ben Gabay <ben.gabay38@gmail.com>
2020-05-30 09:53:34 -0400

    add script for creating banner for terminal

---
+++ terminal/print_ascii_art.sh
@@ -0,0 +1,12 @@
+
+echo '      _  ***                             |.--- *** ---.|'
+echo '     ( `\( ) *   A Prayer to my          || (`\( )/`) ||'
+echo '      `> /~\_I    COMPUTER ANGELS:       ||  > /~\ <  ||'
+echo '      (_/ /\/                            ||  (// \\)  ||'
+echo '        \ \    Guide my keystrokes,      ||___`| |`___||'
+echo '        / /    Keep my programs alive,    `-)--|_|--(-'''
+echo '        \/     Protect me from viruses,    [=== -- o ]--.'
+echo '               Back up my drive.         __'---------'__ \'
+echo '                                        [::::::::::: :::] )'
+echo '                              Amen.       """""""""""""'
+echo '                                                        \_/'

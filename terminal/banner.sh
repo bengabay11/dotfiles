b0a5dd556905ec33d0f292c5d81adb6dd0d7e131
Ben Gabay <ben.gabay38@gmail.com>
2021-10-12 13:27:55 +0300

    combine banner files

--- terminal/banner.sh
+++ terminal/banner.sh
@@ -1,2 +1,13 @@
-./prayer_to_computer_angel.sh
+echo '      _  ***                             |.--- *** ---.|'
+echo '     ( `\( ) *   A Prayer to my          || (`\( )/`) ||'
+echo '      `> /~\_I    COMPUTER ANGELS:       ||  > /~\ <  ||'
+echo '      (_/ /\/                            ||  (// \\)  ||'
+echo '        \ \    Guide my keystrokes,      ||___`| |`___||'
+echo '        / /    Keep my programs alive,    `-)--|_|--(-'''
+echo '        \/     Protect me from viruses,    [=== -- o ]--.'
+echo '               Back up my drive.         __'---------'__ \'
+echo '                                        [::::::::::: :::] )'
+echo '                              Amen.       """""""""""""'
+echo '                                                        \_/'
+
 figlet -f slant "BoonGooboon" | lolcat
--- terminal/launch.sh
+++ terminal/launch.sh
@@ -2,5 +2,4 @@
 
 sudo apt-get install -y figlet
 sudo apt-get install -y lolcat
-cp -f ./terminal/prayer_to_computer_angel.sh ~/
 cat ./terminal/banner.sh >> ~/.bashrc
